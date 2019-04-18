b109969d0173ee96d0091f658a58fa6b65934616
Alden <adderan@ucsc.edu>
2019-04-17 18:27:09 -0700

    Simplify and return to toil script

--- CactusTEAnnotator/findRepeats.py
+++ CactusTEAnnotator/findRepeats.py
@@ -6,7 +6,13 @@ import shutil
 import networkx
 import sys
 
-from sonLib.bioio import fastaRead, fastaWrite, catFiles, reverseComplement,getTempFile
+from toil.job import Job
+from toil.common import Toil
+
+from sonLib.bioio import fastaRead, fastaWrite, catFiles, reverseComplement
+
+def makeURL(path):
+    return "file://%s" % path
 
 import CactusTEAnnotator.treeBuilding as treeBuilding
 
@@ -19,18 +25,33 @@ def catFiles(fileList, target):
                 targetWrite.write("\n")
 
 class RepeatCandidate:
-    def __init__(self, chrom, start, end, family, name, seq, strand):
+    def __init__(self, gffLine):
+        chrom, annotationType, name, start, end, score, strand, a, group = gffLine.split()
         self.chrom = chrom
-        self.start = start
-        self.end = end
+        self.start = int(start)
+        self.end = int(end)
         self.name = name
-        self.seq = seq
         self.strand = strand
+        self.group = group
 
-def getSequence(hal, chrom, start, end, args):
-    fastaLines = subprocess.check_output(["hal2fasta", hal, args.reference, "--sequence", chrom, "--start", str(start), "--length", str(end - start)]).strip().split("\n")
-    sequence = fastaLines[1:]
-    return "\n".join(sequence)
+def getGffLine(chrom, name, strand, start, end, family):
+    return "%s\t%s\t%s\t%d\t%d\t%d\t%s\t%s\t%s\n" % (chrom, "cactus_repeat_annotation", name, start, end, 0, strand, '.', family)
+
+
+def getSequence(hal, chrom, strand, start, end, args):
+    hal2fastaCmd = ["hal2fasta", hal, args.genome, "--sequence", chrom, "--start", str(start), "--length", str(end - start)]
+    fastaLines = subprocess.check_output(hal2fastaCmd).strip().split("\n")
+    sequence = "\n".join(fastaLines[1:])
+    if strand == "-":
+        return reverseComplement(sequence)
+    return sequence
+
+def getFasta(hal, fastaFile, chrom, start, end, args):
+    hal2fastaCmd = ["hal2fasta", hal, args.genome]
+    if end and start and chrom:
+        hal2fastaCmd.extend(["--sequence", chrom, "--start", str(start), "--length", str(end - start)])
+    with open(fastaFile, 'w') as fh:
+        subprocess.check_call(hal2fastaCmd, stdout = fh)
 
 def getRootPath():
     import CactusTEAnnotator
@@ -47,74 +68,90 @@ def highNFraction(seqs):
             return True
     return False
 
-def getInsertions(hal, args):
+def getInsertions(job, halID, args):
     """Extract candidate TE insertions from the Cactus alignment.
         """
-    insertions = []
+    hal = job.fileStore.readGlobalFile(halID)
+    gff = job.fileStore.getLocalTempFile()
     i = 0
-    for bedLine in subprocess.check_output(["halAlignedExtract", "--complement", hal, args.reference]).split("\n"):
-        if len(bedLine.split()) != 3:
-            continue
-        chrom, start, end = bedLine.split()
-        start = int(start)
-        end = int(end)
-        if end - start > args.maxInsertionSize:
-            continue
-        if end - start < args.minInsertionSize:
-            continue
-        sequence = getSequence(hal, chrom, start, end, args)
-        assert len(sequence) > 0
+    with open(gff, 'w') as gffWrite:
+        for bedLine in subprocess.check_output(["halAlignedExtract", "--complement", hal, args.genome]).split("\n"):
+            if len(bedLine.split()) != 3:
+                continue
+            chrom, start, end = bedLine.split()
+            start = int(start)
+            end = int(end)
+            if end - start > args.maxInsertionSize:
+                continue
+            if end - start < args.minInsertionSize:
+                continue
+            sequence = getSequence(hal = hal, chrom = chrom, strand = "+", start = start, end = end, args = args)
+            assert len(sequence) > 0
 
-        if highNFraction(sequence):
-            continue
-        if args.maxInsertions > 0 and i > args.maxInsertions:
-            break
-        forward = RepeatCandidate(chrom=chrom, start=start, end=end, family=None, name=str(i), seq=sequence, strand="+")
-        backward = RepeatCandidate(chrom=chrom, start=start, end=end, family=None, name="%d_comp" % i, seq=reverseComplement(sequence), strand="-")
+            if highNFraction(sequence):
+                continue
+            if args.maxInsertions > 0 and i >= args.maxInsertions:
+                break
+            gffWrite.write(getGffLine(chrom=chrom, start=start, end=end, name=str(i), strand="+", family=str(i)))
+            gffWrite.write(getGffLine(chrom=chrom, start=start, end=end, name=str(i), strand="-", family=str(i)))
+            i = i + 1
 
-        forward.reverseName = backward.name
-        backward.reverseName = forward.name
-        insertions.append(forward)
-        insertions.append(backward)
+    print(sys.stderr, "Found %d insertions on branch" % i)
 
-        i = i + 1
-    print(sys.stderr, "Found %d insertions on branch" % len(insertions))
-    return insertions
+    return job.fileStore.writeGlobalFile(gff)
 
-def runRepeatScout(repeatCandidates, args):
-    seqs = getTempFile(rootDir = args.outDir)
-    writeSequencesToFile(repeatCandidates, seqs)
+def runRepeatScout(job, halID, gffID, seqID, args):
+    hal = job.fileStore.readGlobalFile(halID)
+    gff = job.fileStore.readGlobalFile(gffID)
+    seq = job.fileStore.readGlobalFile(seqID)
 
-    repeatScoutFreqs = os.path.join(args.outDir, "repeat_scout.freq")
+    seqs = job.fileStore.getLocalTempFile()
+    with open(gff, 'r') as gffFile:
+        with open(seqs, 'w') as seqsFile:
+            for line in gffFile:
+                repeatCandidate = RepeatCandidate(line)
+                sequence = getSequence(hal = hal, chrom = repeatCandidate.chrom, strand = repeatCandidate.strand, start = repeatCandidate.start, end = repeatCandidate.end, args = args)
+                seqsFile.write(">%s\n" % repeatCandidate.name)
+                seqsFile.write(sequence)
+
+    repeatScoutFreqs = job.fileStore.getLocalTempFile()
     subprocess.check_call(["build_lmer_table", "-sequence", seqs, "-freq", repeatScoutFreqs])
 
-    repeatScoutLibrary = os.path.join(args.outDir, "repeat_scout_library.fa")
+    repeatScoutLibrary = job.fileStore.getLocalTempFile()
     subprocess.check_call(["RepeatScout", "-sequence", seqs, "-output", repeatScoutLibrary, "-freq", repeatScoutFreqs])
-    return repeatScoutLibrary
 
-def runRepeatMasker(repeatLibrary, genome, args):
-    repeatMaskerOutput = os.path.join(args.outDir, "RepeatMaskerOutput")
-    os.makedirs(repeatMaskerOutput)
-    subprocess.check_call(["RepeatMasker", "-nolow", "-dir", repeatMaskerOutput, "-lib", repeatLibrary, genome])
-    outputGFF = "%s.out" % genome
+    return job.fileStore.writeGlobalFile(repeatScoutLibrary)
 
-    #Parse the GFF so it can be converted to a more standard format
-    repeatFamilies = {}
-    with open(outputGFF, 'r') as outputGFFRead:
-        for i, line in enumerate(outputGFFRead):
-            try:
-                score, div, deletions, insertions, chrom, start, end, left, strand, family, repeatType, a, b, c, d = line.split()
-                repeatCandidate = RepeatCandidate(start = int(start), end = int(end), chrom = chrom, strand = strand, family = family, name = str(i), seq = None)
-                if not family in repeatFamilies:
-                    repeatFamilies[family] = []
-                repeatFamilies[family].append(repeatCandidate)
-            except ValueError:
-                continue
+def parseRepeatMaskerGFF(job, gffID, args):
+    rmaskGffFile = job.fileStore.readGlobalFile(gffID)
+    gffFile = job.fileStore.getLocalTempFile()
+    with open(rmaskGffFile, 'r') as outputGFFRead:
+        with open(gffFile, 'w') as gffWrite:
+            for i, line in enumerate(outputGFFRead):
+                try:
+                    score, div, deletions, insertions, chrom, start, end, left, strand, family, repeatType, a, b, c, d = line.split()
+                    start = int(start) + args.start
+                    end = int(end) + args.start
+                    gffLine = getGffLine(chrom = chrom, start = int(start), end = int(end), name = str(i), strand = strand, family = family)
+                    gffWrite.write("%s" % gffLine)
+                except ValueError:
+                    continue
+    return job.fileStore.writeGlobalFile(gffFile)
 
-    return repeatFamilies
 
-def minhashClustering(repeatCandidates, args):
-    seqs = getTempFile(rootDir = args.outDir)
+def runRepeatMasker(job, repeatLibraryID, seqID, args):
+    repeatLibrary = job.fileStore.readGlobalFile(repeatLibraryID)
+    seq = job.fileStore.readGlobalFile(seqID)
+    repeatMaskerOutput = job.fileStore.getLocalTempDir()
+    subprocess.check_call(["RepeatMasker", "-nolow", "-dir", repeatMaskerOutput, "-lib", repeatLibrary, seq])
+    outputGff = "%s/%s.out" % (repeatMaskerOutput, os.path.basename(seq))
+    job.fileStore.logToMaster("directory contents: %s" % os.listdir(repeatMaskerOutput))
+    outputGffID = job.fileStore.writeGlobalFile(outputGff)
+
+    return job.addChildJobFn(parseRepeatMaskerGFF, gffID = outputGffID, args = args).rv()
+
+def minhashClustering(job, repeatCandidates, args):
+    seqs = job.fileStore.getLocalTempFile()
     with open(seqs, "w") as seqsWrite:
         for seq in repeatCandidates:
             seqsWrite.write(">%s\n" % seq.name)
@@ -130,28 +167,9 @@ def minhashClustering(repeatCandidates, args):
         families.append(family)
     return families
 
-def readGFF(gff, hal, args):
-    elements = []
-    with open(gff, 'r') as gffRead:
-        for line in gffRead:
-            chrom, annotationType, name, start, end, score, strand, a, group = line.split()
-            sequence = getSequence(hal, chrom, int(start), int(end), args)
-            elements.append(RepeatCandidate(chrom = chrom, start = int(start), end = int(end), family = group, name = name, seq = sequence, strand = strand))
-    return elements
-
-def writeGFF(repeatFamilies, args):
-    """Write a set of TE annotations in GFF format.
-    """
-    gff = getTempFile(rootDir = args.outDir)
-    with open(gff, 'w') as gffWrite:
-        for i, family in enumerate(repeatFamilies):
-            for element in repeatFamilies[family]:
-                gffWrite.write("%s\t%s\t%s\t%d\t%d\t%d\t%s\t%s\t%s\n" % (element.chrom, "cactus_repeat_annotation", element.name, element.start, element.end, 0, element.strand, '.', i))
-    return gff
-
 ####Partial order alignment###########
 
-def writeSequencesToFile(repeatCandidates, seqsFile):
+def writeSequencesToFile(gff, seqsFile):
     with open(seqsFile, "w") as seqsWrite:
         for repeatCandidate in repeatCandidates:
             seqsWrite.write(">%s\n" % repeatCandidate.name)
@@ -226,37 +244,49 @@ def joinSubfamiliesByMinhashDistance(repeatFamilies, args):
         newClusters.append(newCluster)
     return newClusters
 
+def repeatScoutRepeatMasker(job, halID, args):
+    hal = job.fileStore.readGlobalFile(halID)
+    genomeFile = job.fileStore.getLocalTempFile()
+    getFasta(hal = hal, fastaFile = genomeFile, chrom = args.chrom, start = args.start, end = args.end, args = args)
+    seqID = job.fileStore.writeGlobalFile(genomeFile)
+    getInsertionsJob = Job.wrapJobFn(getInsertions, halID = halID, args = args)
+
+    repeatScoutJob = Job.wrapJobFn(runRepeatScout, halID = halID, gffID = getInsertionsJob.rv(), seqID = seqID, args = args)
+    repeatMaskerJob = Job.wrapJobFn(runRepeatMasker, repeatLibraryID = repeatScoutJob.rv(), seqID = seqID, args = args)
+
+    job.addChild(getInsertionsJob)
+    getInsertionsJob.addFollowOn(repeatScoutJob)
+    repeatScoutJob.addFollowOn(repeatMaskerJob)
+
+    return repeatMaskerJob.rv()
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("hal", type=str)
-    parser.add_argument("reference", type=str)
-    parser.add_argument("outDir", type=str)
+    parser.add_argument("genome", type=str)
+    parser.add_argument("outGff", type=str)
 
-    parser.add_argument("--referenceSeq", type=str, help="Fasta file containing the reference sequence, to skip extracting it from the HAL file.")
 
     parser.add_argument("--minInsertionSize", type=int, default=100)
     parser.add_argument("--maxInsertionSize", type=int, default=50000)
     parser.add_argument("--maxNFraction", type=float, default=0.1)
+    parser.add_argument("--maxInsertions", type=int, default=None)
 
-    parser.add_argument("--maxInsertions", type=int, default=0)
+    parser.add_argument("--chrom", type=str, default=None)
+    parser.add_argument("--start", type=int, default=None)
+    parser.add_argument("--end", type=int, default=None)
 
+    Job.Runner.addToilOptions(parser)
 
     args = parser.parse_args()
 
-    if os.path.exists(args.outDir):
-        print("Work dir already exists, exiting")
-        exit()
-    else:
-        os.mkdir(args.outDir)
+    with Toil(args) as toil:
+        halID = toil.importFile(makeURL(args.hal))
 
-    insertions = getInsertions(hal=args.hal, args=args)
-
-    repeatLibrary = runRepeatScout(repeatCandidates = insertions, args = args)
-
-    repeatFamilies = runRepeatMasker(repeatLibrary = repeatLibrary, genome = args.referenceSeq, args = args)
-    gff = writeGFF(repeatFamilies = repeatFamilies, args = args)
-
-    shutil.copyfile(gff, os.path.join(args.outDir, "final.gff"))
+        repeatScoutRepeatMaskerJob = Job.wrapJobFn(repeatScoutRepeatMasker, halID = halID, args = args)
+        gffID = toil.start(repeatScoutRepeatMaskerJob)
+        toil.exportFile(gffID, makeURL(args.outGff))
 
 if __name__ == "__main__":
     main()
