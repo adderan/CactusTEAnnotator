4599920e9a76dd10662ef3d6ca75a5c99bb48f45
Alden <adderan@ucsc.edu>
2019-06-11 16:38:57 -0700

    join nearby insertions (incomplete)

--- impl/getTECandidates.cpp
+++ impl/getTECandidates.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string>
 #include "hal.h"
+#include <fstream>
 
 extern "C" {
 #include "sonLib.h"
@@ -11,6 +12,44 @@ extern "C" {
 using namespace hal;
 using namespace std;
 
+
+void printGffEntry(ostream *output, const vector<TopSegmentIteratorConstPtr> &segments, int seqNum, bool forward) {
+
+	if (forward) {
+		for (int i = 0; i < segments.size(); i++) {
+			const Sequence *sequence = segments[i]->getSequence();
+			hal_size_t start = sequence->getStartPosition() + segments[i]->getStartPosition();
+			hal_size_t end = sequence->getStartPosition() + segments[i]->getEndPosition();
+
+			char strand = '+';
+			*output << 
+				sequence->getName() << "\t"
+				"candidate_transposon" << "\t" <<
+				"cte" << seqNum << "_" << i << "\t" << 
+				start << "\t" << 
+				end << "\t" << 
+				"0" << "\t" << 
+				strand << "\t" << 
+				"." << "\t" << 
+				seqNum << endl;
+		}
+	}
+}
+
+void printFastaEntry(ostream *output, const vector<TopSegmentIteratorConstPtr> &segments, int seqNum, bool forward) {
+
+	*output << ">" << "cte" << seqNum << endl;
+	for (int i = 0; i < segments.size(); i++) {
+		const Sequence *sequence = segments[i]->getSequence();
+		string seq;
+		hal_size_t start = sequence->getStartPosition() +segments[i]->getStartPosition();
+		hal_size_t length = segments[i]->getLength();
+		sequence->getSubString(seq, start, length);
+		*output << seq;
+	}
+	*output << endl;
+}
+
 int main(int argc, char **argv) {
 	string halPath;
 	string genomeName;
@@ -18,6 +57,7 @@ int main(int argc, char **argv) {
 	hal_size_t maxLength = 10000;
 	string fastaFilename;
 	string gffFilename;
+	hal_size_t maxDiscardedSequence;
 	int maxSequences = 0;
 
 	CLParserPtr parser = hdf5CLParserInstance(false);
@@ -29,6 +69,8 @@ int main(int argc, char **argv) {
 	parser->addOption("maxLength", "Maximum length of candidate TEs", 10000);
 	parser->addOption("maxSequences", "Maximium number of candidate TEs to output", 0);
 
+	parser->addOption("maxDiscardedSequence", "Maximum distance to join multiple HAL insertions as one candidate TE", 500);
+
 	try {
 		parser->parseOptions(argc, argv);
 		halPath = parser->getArgument<string>("halPath");
@@ -38,6 +80,7 @@ int main(int argc, char **argv) {
 		minLength = parser->getOption<hal_size_t>("minLength");
 		maxLength = parser->getOption<hal_size_t>("maxLength");
 		maxSequences = parser->getOption<int>("maxSequences");
+		maxDiscardedSequence = parser->getOption<hal_size_t>("maxDiscardedSequence");
 	}
 	catch (exception& e) {
 		cerr << e.what() << endl;
@@ -55,50 +98,61 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	FILE *fastaFile = NULL;
-	FILE *gffFile = stdout;
+	ostream *gffFile = &cout;
+	ofstream gffStream;
+	ostream *fastaFile = NULL;
+	ofstream fastaStream;
 	if (fastaFilename != "") {
-		fastaFile = fopen(fastaFilename.c_str(), "w");
+		fastaStream.open(fastaFilename);
+		fastaFile = &fastaStream;
 	}
 
 	if (gffFilename != "") {
-		gffFile = fopen(gffFilename.c_str(), "w");
+		gffStream.open(gffFilename);
+		gffFile = &gffStream;
 	}
 
 	TopSegmentIteratorConstPtr topSeg = genome->getTopSegmentIterator();
 	TopSegmentIteratorConstPtr endSeg = genome->getTopSegmentEndIterator();
 
-	DNAIteratorConstPtr dnaIt = genome->getDNAIterator();
 	int i = 0;
-	for (; topSeg->equals(endSeg) == false; topSeg->toRight()) {
-		if (topSeg->hasParent()) continue;
-		if (topSeg->getLength() > maxLength) continue;
-		if (topSeg->getLength() < minLength) continue;
-
-		const Sequence *sequence = topSeg->getSequence();
-		int start = topSeg->getStartPosition() - sequence->getStartPosition();
-		int end = topSeg->getEndPosition() - sequence->getStartPosition();
-
-		//Print the GFF line
-		fprintf(gffFile, "%s\tcandidate_transposon\tcte_%d\t%d\t%d\t0\t+\t.\t%d\n", sequence->getName().c_str(), i, start, end, i);
-		fprintf(gffFile, "%s\tcandidate_transposon\tcte_%d_comp\t%d\t%d\t-\t.\t%d\n", sequence->getName().c_str(), i, start, end, i);
-
-		if (fastaFile) {
-			string seqBuffer;
-			sequence->getSubString(seqBuffer, start, topSeg->getLength());
-			fprintf(fastaFile, ">cte_%d\n", i);
-			fprintf(fastaFile, "%s\n", seqBuffer.c_str());
-
-			char *reverseCompSeq = stString_reverseComplementString(seqBuffer.c_str());
-			fprintf(fastaFile, ">cte_%d_comp\n", i);
-			fprintf(fastaFile, "%s\n", reverseCompSeq);
-			free(reverseCompSeq);
+	vector<TopSegmentIteratorConstPtr> segments;
+	while(true) {
+		hal_size_t discardedSequence = 0;
+		hal_size_t seqLength = 0;
+		//scan to the next insert
+		while(!topSeg->equals(endSeg) && topSeg->hasParent()) {
+			topSeg->toRight();
 		}
 
+
+		while (true) {
+			if (topSeg->hasParent()) {
+				discardedSequence += topSeg->getLength();
+				if (discardedSequence > maxDiscardedSequence) break;
+			}
+			else {
+				segments.push_back(topSeg->copy());
+				seqLength += topSeg->getLength();
+			}
+		}
+
+		if (seqLength < minLength) continue;
+		if (seqLength > maxLength) continue;
+
+		printGffEntry(gffFile, segments, i, true);
+
+		if (fastaFile) {
+			//forward strand
+			printFastaEntry(fastaFile, segments, i, true);
+		}
+
+		segments.clear();
 		i++;
 		if (maxSequences > 0 && i >= maxSequences) break;
+		if (topSeg->equals(endSeg)) break;
 	}
 
-	if (fastaFile) fclose(fastaFile);
-	if (gffFile) fclose(gffFile);
+	if (fastaFile) fastaStream.close();
+	if (gffFile) gffStream.close();
 }
