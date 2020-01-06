1507d4e1d3301823281c77afa6c17ea906fd308f
Alden <adderan@ucsc.edu>
2020-01-05 18:18:46 -0800

    -

--- CactusTEAnnotator/scoreGFF.py
+++ CactusTEAnnotator/scoreGFF.py
@@ -26,92 +26,115 @@ def readGff(filename):
     return annotations
 
 
-def overlap(target, query):
-    assert target.start < target.end
+def overlap(reference, query):
+    assert reference.start < reference.end
     assert query.start < query.end
-    if target.start > query.end or query.start > target.end:
+    if reference.start > query.end or query.start > reference.end:
         return 0.0
-    overlapStart = target.start if target.start > query.start else query.start
-    overlapEnd = target.end if target.end < query.end else query.end
+    overlapStart = reference.start if reference.start > query.start else query.start
+    overlapEnd = reference.end if reference.end < query.end else query.end
 
     overlappingBases = overlapEnd - overlapStart + 1
-    overlapFraction = float(overlappingBases)/float(target.end - target.start)
+    overlapFraction = float(overlappingBases)/float(reference.end - reference.start)
     return overlapFraction
-
+    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--queryGff", type=str)
-    parser.add_argument("--targetGff", type=str)
+    parser.add_argument("--referenceGff", type=str)
     parser.add_argument("--out", type=str)
+    parser.add_argument("--skipConcordance", action="store_true", default=False)
 
     args = parser.parse_args()
 
     if os.path.exists(args.out):
-        print("Output folder %s already exists.", file=sys.stderr)
+        print("Output folder %s already exists." % args.out, file=sys.stderr)
+        exit(1)
+    args.out = os.path.abspath(args.out)
+    os.mkdir(args.out)
 
     queryAnnotations = readGff(args.queryGff)
-    targetAnnotations = readGff(args.targetGff)
+    referenceAnnotations = readGff(args.referenceGff)
 
     queryAnnotations.sort(key = lambda x: (x.start, x.end))
-    targetAnnotations.sort(key = lambda x: (x.start, x.end))
+    referenceAnnotations.sort(key = lambda x: (x.start, x.end))
 
     matchingAnnotations = []
-    targetIter = iter(targetAnnotations)
+    referenceIter = iter(referenceAnnotations)
     queryIter = iter(queryAnnotations)
-    target = next(targetIter)
+    reference = next(referenceIter)
     query = next(queryIter)
-    while target and query:
-        if overlap(query, target) > 0.5:
-            matchingAnnotations.append((query, target))
-            query.match = target
-            target.match = query
-            target = next(targetIter, None)
+    while reference and query:
+        if overlap(query, reference) > 0.5:
+            matchingAnnotations.append((query, reference))
+            query.match = reference
+            reference.match = query
+            reference = next(referenceIter, None)
             query = next(queryIter, None)
-        elif target.start < query.start:
-            target = next(targetIter, None)
+        elif reference.start < query.start:
+            reference = next(referenceIter, None)
         else:
             query = next(queryIter, None)
 
     print("Found %d overlapping annotations" % len(matchingAnnotations), file=sys.stderr)
 
-    nConcordant = 0
-    total = 0
-    for i in range(len(matchingAnnotations)):
-        if i % 50 != 0:
-            continue
-        target_i, query_i = matchingAnnotations[i]
-        for j in range(i):
-            target_j, query_j = matchingAnnotations[j]
-            a = target_i.family == target_j.family
-            b = query_i.family == query_j.family
-            if ((a and b) or ((not a) and (not b))):
-                nConcordant = nConcordant + 1
-            total = total + 1
-    concordance = float(nConcordant)/float(total)
-    print("Concordance = %f" % concordance, file=sys.stderr)
+    if not args.skipConcordance:
+        nConcordant = 0
+        total = 0
+        for i in range(len(matchingAnnotations)):
+            if i % 50 != 0:
+                continue
+            reference_i, query_i = matchingAnnotations[i]
+            for j in range(i):
+                reference_j, query_j = matchingAnnotations[j]
+                a = reference_i.family == reference_j.family
+                b = query_i.family == query_j.family
+                if ((a and b) or ((not a) and (not b))):
+                    nConcordant = nConcordant + 1
+                total = total + 1
+        concordance = float(nConcordant)/float(total)
+        print("Concordance = %f" % concordance, file=sys.stderr)
 
 
-    if args.familyCoverageStats:
-        targetFamilies = {}
-        for target in targetAnnotations:
-            if target.family not in targetFamilies:
-                targetFamilies[target.family] = []
-            targetFamilies[target.family].append(target)
-        familyStats = []
-        for family in targetFamilies:
-            coveredRepeatCopies = 0
-            for repeatCopy in targetFamilies[family]:
-                if repeatCopy.match is not None:
-                    coveredRepeatCopies = coveredRepeatCopies + 1
-            familyCoverage = float(coveredRepeatCopies)/float(len(targetFamilies[family]))
+    referenceFamilies = {}
+    for reference in referenceAnnotations:
+        if reference.family not in referenceFamilies:
+            referenceFamilies[reference.family] = []
+        referenceFamilies[reference.family].append(reference)
+    familyStats = []
+    for family in referenceFamilies:
+        coveredRepeatCopies = 0
+        for repeatCopy in referenceFamilies[family]:
+            if repeatCopy.match is not None:
+                coveredRepeatCopies = coveredRepeatCopies + 1
+        familyCoverage = float(coveredRepeatCopies)/float(len(referenceFamilies[family]))
 
-            averageLength = float(sum([copy.end - copy.start for copy in targetFamilies[family]]))/float(len(targetFamilies[family]))
-            concordance = 0
-            
-            familyStats.append((family, len(targetFamilies[family]), familyCoverage, averageLength))
-        familyStats.sort(key = lambda x: x[1], reverse=True)
-        with open(args.familyCoverageStats, "w") as coverageStatsFile:
-            for familyInfo in familyStats:
-                print("\t".join([str(item) for item in familyInfo]), file=coverageStatsFile)
+        averageLength = float(sum([copy.end - copy.start for copy in referenceFamilies[family]]))/float(len(referenceFamilies[family]))
+        concordance = 0
+        
+        familyStats.append((family, len(referenceFamilies[family]), coveredRepeatCopies, familyCoverage, averageLength))
+    familyStats.sort(key = lambda x: x[1], reverse=True)
+    familyCoverageStats = os.path.join(args.out, "family_coverage.txt")
+    with open(familyCoverageStats, "w") as coverageStatsFile:
+        for familyInfo in familyStats:
+            print("\t".join([str(item) for item in familyInfo]), file=coverageStatsFile)
+    
+    #Plot of number of repeat copies per family and how
+    #many are covered by the query annotations
+    nFamilies = 20
+    familyNames = [stats[0] for stats in familyStats]
+    familySize = [stats[1] for stats in familyStats]
+    coveredRepeatCopies = [stats[2] for stats in familyStats]
+    pyplot.bar(familyNames[:nFamilies], familySize[:nFamilies], label="RepeatMasker")
+    pyplot.bar(familyNames[:nFamilies], coveredRepeatCopies[:nFamilies], label="TE Annotations from Cactus")
+    pyplot.ylabel("Number of copies")
+    pyplot.xlabel('RepBase Family')
+    pyplot.title("Coverage of Cactus TE annotations on RepeatMasker families")
+    pyplot.xticks(rotation=45)
+    pyplot.legend()
+    pyplot.savefig(os.path.join(args.out, "family_coverage.png"))
+    pyplot.clf()
+
+
 if __name__ == "__main__":
     main()
